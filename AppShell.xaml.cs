904bfbd113ab70330ce7927e7d0edd8da8dab05d
Sugey Ramirez <al22760593@ite.edu.mx>
2024-11-09 21:59:55 -0800

    Se subió el proyecto

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ProyectoRecetas
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ProyectoRecetas
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
