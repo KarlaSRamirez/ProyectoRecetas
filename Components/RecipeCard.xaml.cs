51a8b6ea9c5b10ed0bcedec2512b96e728bd554c
JoskaSF <joska@gmail.com>
2024-11-29 14:10:53 -0800

    Enlace de RealizarReceta y envio de parametros individuales

--- Components/RecipeCard.xaml.cs
+++ Components/RecipeCard.xaml.cs
@@ -1,10 +1,20 @@
+using ProyectoRecetas.Views;
+
 namespace ProyectoRecetas.Components;
 
 public partial class RecipeCard : ContentView
 {
+
+	private string _title;
+	private string _instructions;
+	private string _ingredients;
 	public RecipeCard(string name, string instruction, string ingredients)
 	{
 		InitializeComponent();
+
+		_title = name;
+		_instructions = instruction;
+		_ingredients = ingredients;
 
 		RecipeTitle.Text = name;
 		RecipeIngredients.Text = ingredients;
@@ -12,6 +22,6 @@ public partial class RecipeCard : ContentView
 
 	private async void CookButton(object sender, EventArgs e)
 	{
-		await Navigation.PushAsync(new RealizarRecetaPage());
+		await Navigation.PushAsync(new RealizarRecetaPage(_title, _instructions, _ingredients));
 	}
 }
--- Views/RealizarRecetaPage.xaml.cs
+++ Views/RealizarRecetaPage.xaml.cs
@@ -2,7 +2,7 @@ namespace ProyectoRecetas.Views;
 
 public partial class RealizarRecetaPage : ContentPage
 {
-	public RealizarRecetaPage()
+	public RealizarRecetaPage(string name, string instruction, string ingredients)
 	{
 		InitializeComponent();
 	}
