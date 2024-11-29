b3011e3bba34e311198484da754a7044ceb41dd5
Sugey Ramirez <al22760593@ite.edu.mx>
2024-11-29 14:23:15 -0800

    Visualiazar la receta completa en realizar recetaPage

--- Views/RealizarRecetaPage.xaml.cs
+++ Views/RealizarRecetaPage.xaml.cs
@@ -5,5 +5,14 @@ public partial class RealizarRecetaPage : ContentPage
 	public RealizarRecetaPage(string name, string instruction, string ingredients)
 	{
 		InitializeComponent();
-	}
+        NombreReceta.Text = name;
+        InstruccionesReceta.Text = instruction;
+        IngredientesReceta.Text = ingredients;
+    }
+
+    private async void HechoButton(object sender, EventArgs e)
+    {
+        await Navigation.PopToRootAsync();
+    }
+
 }
